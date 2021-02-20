8035d3d41db691aab3eb682725b2853c0e3dde45
josiahdc <josiah.chapman@gmail.com>
2021-02-20 13:11:34 -0800

    initialized repo

---
+++ compile_flatbuffers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+flatc --cpp --python --scoped-enums -o generated/ \
+  src/audioProcessing/AudioPacket.fbs \
+  src/audioProcessing/onset/Onset.fbs \
+  src/audioProcessing/onset/OnsetMethod.fbs \
+  src/audioProcessing/onset/OnsetProcessorParameters.fbs
