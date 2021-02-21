b04f81edd5a6bc7d6f191f7bc1252c13f2ee47b6
josiahdc <josiah.chapman@gmail.com>
2021-02-20 17:42:08 -0800

    added generated onset aggregate file

--- compile_flatbuffers.sh
+++ compile_flatbuffers.sh
@@ -3,5 +3,6 @@
 flatc --cpp --python --scoped-enums -o generated/ \
   src/audioProcessing/AudioPacket.fbs \
   src/audioProcessing/onset/Onset.fbs \
+  src/audioProcessing/onset/OnsetAggregate.fbs \
   src/audioProcessing/onset/OnsetMethod.fbs \
   src/audioProcessing/onset/OnsetProcessorParameters.fbs
