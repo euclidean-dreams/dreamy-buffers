e0fd8e6f4d62fa31e99e34db53b62522b7ade95c
josiahdc <josiah.chapman@gmail.com>
2021-02-22 14:15:22 -0800

    added audio attributes

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 
 FLATBUFFERS_SOURCE_FILES="$(find src/ -type f -name "*.fbs" -printf "%p " | xargs)"
 # shellcheck disable=SC2086
-flatc --cpp --scoped-enums -o compiled/cpp/ ${FLATBUFFERS_SOURCE_FILES}
+flatc --cpp --scoped-enums -o compiled/cpp/ -I src/ ${FLATBUFFERS_SOURCE_FILES}
 # shellcheck disable=SC2086
-flatc --python --scoped-enums -o compiled/python/ ${FLATBUFFERS_SOURCE_FILES}
+flatc --python --scoped-enums -o compiled/python/ -I src/ ${FLATBUFFERS_SOURCE_FILES}
