b72ac41ede6b19c02970c4e1288f2725aa02213d
josiahdc <josiah.chapman@gmail.com>
2021-03-30 13:12:19 -0700

    combined headers into single header

--- compile.sh
+++ compile.sh
@@ -5,3 +5,5 @@ FLATBUFFERS_SOURCE_FILES="$(find src/ -type f -name "*.fbs" -printf "%p " | xarg
 flatc --cpp --scoped-enums -o compiled/cpp/ -I src/ ${FLATBUFFERS_SOURCE_FILES}
 # shellcheck disable=SC2086
 flatc --python --scoped-enums -o compiled/python/ -I src/ ${FLATBUFFERS_SOURCE_FILES}
+
+python3 generate_single_header.py
